d85af3bef54ecf78135166ccae1dbe41e66f45df
温皓宇 <2792689489@qq.com>
2026-07-10 17:54:55 +0800

    过滤待装载订单：与已完成一样不发送前端

--- src/main/java/com/jushen/digitaltwin/townroad/TownRoadMiddleLayer.java
+++ src/main/java/com/jushen/digitaltwin/townroad/TownRoadMiddleLayer.java
@@ -122,8 +122,8 @@ public class TownRoadMiddleLayer {
                 continue;
             }
 
-            if ("已完成".equals(order.status())) {
-                continue; // 已完成的不发送前端
+            if ("已完成".equals(order.status()) || "待装载".equals(order.status())) {
+                continue; // 已完成/待装载的不发送前端
             }
 
             shortHaulOrders.add(order);
@@ -598,7 +598,7 @@ public class TownRoadMiddleLayer {
 
         List<TownRoadOrder> orders = flatOrderMap.values()
                 .stream()
-                .filter(order -> !"已完成".equals(order.status()))
+                .filter(order -> !"已完成".equals(order.status()) && !"待装载".equals(order.status()))
                 .sorted(orderComparator())
                 .map(this::toTownRoadOrder)
                 .toList();
